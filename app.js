5482e912bb44be0ee8b10fd1cb18ad40d088a982
Rakesh Vishwakarma <rakesh830566@gmail.com>
2021-05-09 10:07:13 +0530

    Working With The DOM and ES6 Variables

---
+++ app.js
@@ -0,0 +1,19 @@
+const linkCategory = document.querySelector("#linkCategory");
+
+let linkCategories = [];
+
+linkCategory.addEventListener("keydown", (event) => {
+	if (event.keyCode === 188) {
+		event.preventDefault();
+
+		linkCategories.push(linkCategory.value);
+		linkCategory.value = "";
+
+		// ? Display the updated categories
+		displayLinkCategories();
+	}
+});
+
+function displayLinkCategories() {
+	console.log("Displaying Link Categories");
+}
