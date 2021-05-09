314282d094eecf9262f2145df77208e79583db79
Rakesh Vishwakarma <rakesh830566@gmail.com>
2021-05-09 12:04:32 +0530

    ES6 Arrow Functions and the This Keyword

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const linkCategory = document.querySelector("#linkCategory");
+const submitButton = document.querySelector("#submitButton");
 
 let linkCategories = [];
+let links = [];
 
 linkCategory.addEventListener("keydown", (event) => {
 	if (event.keyCode === 188) {
 		event.preventDefault();
-
 		linkCategories.push(linkCategory.value);
 		linkCategory.value = "";
 
@@ -17,3 +18,28 @@ linkCategory.addEventListener("keydown", (event) => {
 function displayLinkCategories() {
 	console.log("Displaying Link Categories");
 }
+
+submitButton.addEventListener("click", (event) => {
+	// ? Stop form from submitting
+	event.preventDefault();
+
+	const title = linkTitle.value;
+	const url = linkUrl.value;
+	const categories = linkCategories;
+
+	const newLink = {
+		title,
+		url,
+		categories,
+	};
+
+	// ? Push new link to array
+	links.push(newLink);
+
+	linkTitle.value = "";
+	linkUrl.value = "";
+	linkCategory.value = "";
+	linkCategories = [];
+
+	displayLinkCategories();
+});
